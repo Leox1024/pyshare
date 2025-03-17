14d23fa157cac43e040ac521880d3a3d35fed00e
l3ox64 <github@leox.me>
2025-03-17 19:57:51 +0100

    fix-requirements

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import inspect
 from datetime import datetime
 from flask_validator import ValidateEmail, ValidateString, ValidateCountry
 from sqlalchemy.orm import validates
-from werkzeug.security import generate_password_hash, check_password_hash
+from argon2 import PasswordHasher  # Import Argon2 PasswordHasher
 from flask_login import UserMixin
 from src import db
 
@@ -18,16 +18,24 @@ class Account(db.Model, UserMixin):
     created_at = db.Column(db.DateTime(timezone=True), default=datetime.utcnow)
     updated_at = db.Column(db.DateTime(timezone=True), default=datetime.utcnow, onupdate=datetime.utcnow)
 
+    # Initialize the Argon2 Password Hasher
+    ph = PasswordHasher()
+
     @classmethod
     def __declare_last__(cls):
         ValidateEmail(Account.email, True, True, "Invalid email address")
         ValidateString(Account.username, True, True, "Username must be a valid string")
 
     def set_password(self, password):
-        self.hashed_password = generate_password_hash(password)
+        """Hash the password using Argon2 and store the hashed password"""
+        self.hashed_password = self.ph.hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.hashed_password, password)
+        """Check the provided password against the stored hashed password"""
+        try:
+            return self.ph.verify(self.hashed_password, password)
+        except:
+            return False  # If the password doesn't match, return False
 
     def toDict(self):
         return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
